14a85d76fc3827c01a1d55ef2d15adf9663f3d14
Japka-live <mr.ikatsyuba1@gmail.com>
2024-11-25 15:01:57 +1000

    Update Testssh2.sh

--- Testssh2.sh
+++ Testssh2.sh
@@ -15,6 +15,16 @@ SSH_PASS="resu"
 ROOT_PASS="toor"
 
 # Подключение по SSH и выполнение команд
-sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HQ" << EOF
- echo "$ROOT_PASS" | su - -c "apt-get update && apt-get install -y isc-dhcp-server"
+echo "$ROOT_PASS" | su - -c "
+apt-get update && apt-get install -y isc-dhcp-server
+sed -i 's/^INTERFACESv4=\"\"/INTERFACESv4=\"ens37\"/' /etc/default/isc-dhcp-server
+
+# Редактирование файла /etc/dhcp/dhcpd.conf
+sed -i '/option-domain-name-servers/a\\n\\nsubnet 192.168.10.0 netmask 255.255.255.248 {\\n  range 192.168.10.4 192.168.10.6;\\n  option routers 192.168.10.1;\\n}' /etc/dhcp/dhcpd.conf
+
+# Перезапуск и включение isc-dhcp-server
+systemctl restart isc-dhcp-server
+systemctl start isc-dhcp-server
+systemctl enable isc-dhcp-server
+"
 EOF
