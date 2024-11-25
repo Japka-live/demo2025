d06d107445b9964a904550bef8781e1d4b4064f3
Japka-live <mr.ikatsyuba1@gmail.com>
2024-11-25 14:11:00 +1000

    Update Testssh2.sh

--- Testssh2.sh
+++ Testssh2.sh
@@ -9,13 +9,12 @@ then
 fi
 
 # Переменные
-SSH_HQ-RTR="192.168.10.1"
+SSH_HQ="192.168.10.1"
 SSH_USER="user"
 SSH_PASS="resu"
 ROOT_PASS="toor"
 
 # Подключение по SSH и выполнение команд
-sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HQ-RTR" << EOF
-echo "$ROOT_PASS" | su - 
-apt instal isc-dhcp-server
+sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HQ" << EOF
+ echo "$ROOT_PASS" | su - -c "apt-get update && apt-get install -y isc-dhcp-server"
 EOF
