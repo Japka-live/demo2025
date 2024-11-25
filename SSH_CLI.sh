c385241fe6cb0a9253f9c5500a2d24fe78ee01ad
Japka-live <mr.ikatsyuba1@gmail.com>
2024-11-25 20:30:29 +1000

    Update SSH_CLI.sh

--- SSH_CLI.sh
+++ SSH_CLI.sh
@@ -8,15 +8,6 @@ then
     apt-get install -y sshpass
 fi
 
-# Переменные
-SSH_ISP="10.10.10.1"
-SSH_HQRTR="10.10.10.2"
-SSH_BRRTR="10.10.10.6"
-SSH_HQSRV="192.168.10.3"
-SSH_BRSRV="192.168.30.3"
-SSH_USER="user"
-SSH_PASS="resu"
-ROOT_PASS="toor"
 
 #Переименование и создание пользователя Admin на CLI
 hostnamectl hostname CLI
