114ec7b921f2348ad524f3f60a1151a008a1ed8b
Japka-live <mr.ikatsyuba1@gmail.com>
2024-11-25 17:49:25 +1000

    Update and rename dhcp.sh to DHCP.sh

--- DHCP.sh
+++ DHCP.sh
@@ -6,7 +6,7 @@ apt-get install -y isc-dhcp-server
 
 # Редактирование файла /etc/default/isc-dhcp-server
 sed -i 's/^INTERFACESv4=".*"/INTERFACESv4="ens37"/' /etc/default/isc-dhcp-server
-
+sleep 2
 # Редактирование файла /etc/dhcp/dhcpd.conf
 sed -i '/option-domain-name-servers/a\\n\\nsubnet 192.168.10.0 netmask 255.255.255.248 {\\n  range 192.168.10.4 192.168.10.6;\\n  option routers 192.168.10.1;\\n}' /etc/dhcp/dhcpd.conf
 
