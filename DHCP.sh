7b49095a7ad6f4c9161973c276116935bd39ee81
Japka-live <mr.ikatsyuba1@gmail.com>
2024-11-25 17:54:14 +1000

    Update DHCP.sh

--- DHCP.sh
+++ DHCP.sh
@@ -6,9 +6,12 @@ apt-get install -y isc-dhcp-server
 
 # Редактирование файла /etc/default/isc-dhcp-server
 sed -i 's/^INTERFACESv4=".*"/INTERFACESv4="ens37"/' /etc/default/isc-dhcp-server
-sleep 2
+
 # Редактирование файла /etc/dhcp/dhcpd.conf
-sed -i '/option-domain-name-servers/a\\n\\nsubnet 192.168.10.0 netmask 255.255.255.248 {\\n  range 192.168.10.4 192.168.10.6;\\n  option routers 192.168.10.1;\\n}' /etc/dhcp/dhcpd.conf
+echo -e "\nsubnet 192.168.10.0 netmask 255.255.255.248 {" >> /etc/dhcp/dhcpd.conf
+echo -e "  range 192.168.10.4 192.168.10.6;" >> /etc/dhcp/dhcpd.conf
+echo -e "  option routers 192.168.10.1;" >> /etc/dhcp/dhcpd.conf
+echo -e "}" >> /etc/dhcp/dhcpd.conf
 
 # Перезапуск isc-dhcp-server
 systemctl restart isc-dhcp-server
