5a921e8e9527731b954b06dbae07448f7692a142
Japka-live <mr.ikatsyuba1@gmail.com>
2025-05-16 21:53:11 +1000

    Create NFS-client.sh

---
+++ NFS-client.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "===> Установка NFS-клиентских утилит..."
+apt update
+apt install -y nfs-common
+
+echo "===> Создание точки монтирования /mnt/nfs..."
+mkdir -p /mnt/nfs
+
+echo "===> Монтирование NFS-ресурса..."
+mount 192.168.10.3:/raid5/nfs /mnt/nfs
+
+echo "===> Проверка смонтированных файловых систем..."
+df -h
+sleep 2
+
+echo "===> Добавление записи в /etc/fstab..."
+echo "192.168.10.3:/raid5/nfs /mnt/nfs nfs defaults 0 0" >> /etc/fstab
+
+echo "===> Повторное монтирование всех файловых систем..."
+mount -a
+
+echo "===> Перезагрузка системы..."
+reboot
