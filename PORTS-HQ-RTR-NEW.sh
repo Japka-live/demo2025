9fe6af9627bec0508d0b169e3cb56f0b68b825aa
Japka-live <mr.ikatsyuba1@gmail.com>
2025-06-03 19:43:18 +1000

    Update PORTS-HQ-RTR-NEW.sh

--- PORTS-HQ-RTR-NEW.sh
+++ PORTS-HQ-RTR-NEW.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Указываем IP-адрес HQ-SRV
-HQ_SRV_IP="172.16.4.1"
+# Укажите IP-адрес HQ-SRV ниже
+HQ_SRV_IP="192.168.10.3"
 
-echo "===> Проброс порта 80 на ${HQ_SRV_IP}:80..."
+echo "===> Настройка iptables для проброса портов к HQ-SRV ($HQ_SRV_IP)..."
+
+# Проброс порта 80 на HQ-SRV:80 (для Moodle)
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${HQ_SRV_IP}:80
 iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 80 -j ACCEPT
 
-echo "===> Проброс порта 3015 на ${HQ_SRV_IP}:3015..."
+# Проброс порта 3015 на HQ-SRV:3015
 iptables -t nat -A PREROUTING -p tcp --dport 3015 -j DNAT --to-destination ${HQ_SRV_IP}:3015
 iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 3015 -j ACCEPT
 
-echo "===> Сохранение правил через netfilter-persistent..."
+# Сохраняем правила
 netfilter-persistent save
 
-echo "===> Готово. Порт 2024 проброшен на ${HQ_SRV_IP}:2024"
+echo "===> Проброс портов завершен."
