efd74a69fa8a1c564dd1b8a73ac529244b6b5ecb
Japka-live <mr.ikatsyuba1@gmail.com>
2025-06-03 19:16:09 +1000

    Create PORTS-HQ-RTR-NEW.sh

---
+++ PORTS-HQ-RTR-NEW.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Указываем IP-адрес HQ-SRV
+HQ_SRV_IP="192.168.10.3"
+
+echo "===> Проброс порта 80 на ${HQ_SRV_IP}:80..."
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${HQ_SRV_IP}:80
+iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 80 -j ACCEPT
+
+echo "===> Проброс порта 3015 на ${HQ_SRV_IP}:3015..."
+iptables -t nat -A PREROUTING -p tcp --dport 3015 -j DNAT --to-destination ${HQ_SRV_IP}:3015
+iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 3015 -j ACCEPT
+
+echo "===> Сохранение правил через netfilter-persistent..."
+netfilter-persistent save
+
+echo "===> Готово. Порт 2024 проброшен на ${HQ_SRV_IP}:2024"
