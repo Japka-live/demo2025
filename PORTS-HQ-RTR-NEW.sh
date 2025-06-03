f9db5153888fb85e763ac6457aa1a0b0cdb17a5a
Japka-live <mr.ikatsyuba1@gmail.com>
2025-06-03 19:47:54 +1000

    Update PORTS-HQ-RTR-NEW.sh

--- PORTS-HQ-RTR-NEW.sh
+++ PORTS-HQ-RTR-NEW.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Укажи IP HQ-SRV и внешний интерфейс маршрутизатора
+# Укажите IP-адрес HQ-SRV ниже
 HQ_SRV_IP="192.168.10.3"
-WAN_IFACE="eth0"  # замените на свой внешний интерфейс
 
-echo "===> Проброс портов и NAT для HQ-SRV ($HQ_SRV_IP) через $WAN_IFACE..."
+echo "===> Настройка iptables для проброса портов к HQ-SRV ($HQ_SRV_IP)..."
 
-# Проброс порта 80
+# Проброс порта 80 на HQ-SRV:80 (для Moodle)
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${HQ_SRV_IP}:80
 iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 80 -j ACCEPT
 
-# Проброс порта 3015
+# Проброс порта 3015 на HQ-SRV:3015
 iptables -t nat -A PREROUTING -p tcp --dport 3015 -j DNAT --to-destination ${HQ_SRV_IP}:3015
 iptables -A FORWARD -p tcp -d ${HQ_SRV_IP} --dport 3015 -j ACCEPT
-
-# NAT для выхода в интернет
-iptables -t nat -A POSTROUTING -o ${WAN_IFACE} -j MASQUERADE
 
 # Сохраняем правила
 netfilter-persistent save
 
-echo "===> Настройка завершена."
+echo "===> Проброс портов завершен."
